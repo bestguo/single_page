63719fdaf3fe74559681cd925de27b8a15b86dfa
bestguo <1390397356@qq.com>
2015-10-12 15:54:34 +0800

    添加Javascript脚本实现获取数据更新文档结构

---
+++ javascripts/dribbble.js
@@ -0,0 +1,12 @@
+/**
+ * Load Dribbble shots
+ *
+ * This script users Dribbble's API
+ * to fetch the latest shots
+ * from a specialied Dribbble user
+ */
+ 
+ // What user are we using?
+ // 
+ var dribbbleUsername='jimniels';
+ var html='';
