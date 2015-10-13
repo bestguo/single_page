3679a76f09a7f47d93d687f1c05c7513c9952e22
bestguo <1390397356@qq.com>
2015-10-13 10:56:07 +0800

    添加Json数据处理代码
    实现在DOM中添加数据列表的功能。

--- javascripts/dribbble.js
+++ javascripts/dribbble.js
@@ -10,3 +10,21 @@
  // 
  var dribbbleUsername='jimniels';
  var html='';
+
+ // Load the shots via Dribbble's API
+
+$.getJSON("http://api.dribbble.com/players/"+dribbbleUsername+"/shots?callback=?",function(data){
+	// Do somthing with the result here
+	// how many shots we're displaying on the page
+	var numberOfShots=10;
+
+	// Loop over the results, generating the HTML for each <li> item
+	// 
+	for (var i =0; i <numberOfShots; i++) {
+		html +='<li>';
+		html +='<a href="'+data.shots[i].url+'">';
+		html +='<img src="'+data.shots[i].image_url+'" alt="'+data.shots[i].title+'" />';
+		html +='</a>';
+		html +='</li>';
+	};
+})
