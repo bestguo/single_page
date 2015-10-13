cee86b5713aefcceb70066f0b8d50576e7760c8d
bestguo <1390397356@qq.com>
2015-10-13 17:51:55 +0800

    更新js

--- javascripts/dribbble.js
+++ javascripts/dribbble.js
@@ -1,33 +1,37 @@
-/**
- * Load Dribbble shots
+/*
+ * LOAD DRIBBBLE SHOTS
  *
- * This script users Dribbble's API
+ * This script uses Dribbble's API
  * to fetch the latest shots
- * from a specialied Dribbble user
+ * from a speficied Dribbble user
  */
- 
- // What user are we using?
- // 
- var dribbbleUsername='jimniels';
- var html='';
 
- // Load the shots via Dribbble's API
+// What user are we using?
+var dribbbleUsername = 'yoga';
 
-$.getJSON("http://api.dribbble.com/players/"+dribbbleUsername+"/shots?callback=?",function(data){
-	// Do somthing with the result here
-	// how many shots we're displaying on the page
-	var numberOfShots=10;
+// Variable to hold the HTML we'll generate
+var html = '';
 
-	// Loop over the results, generating the HTML for each <li> item
-	// 
-	for (var i =0; i <numberOfShots; i++) {
-		html +='<li>';
-		html +='<a href="'+data.shots[i].url+'">';
-		html +='<img src="'+data.shots[i].image_url+'" alt="'+data.shots[i].title+'" />';
-		html +='</a>';
-		html +='</li>';
-	};
-})
+// Load the shots via Dribbble's API
+$.getJSON("http://api.dribbble.com/players/"+ dribbbleUsername +"/shots?callback=?", function(data) {
 
-// Insert the generated HTML to the DOM
-$('shots').html(html);
+    // how many shots we're displaying on the page
+    var numberOfShots = 10;
+
+    // Loop over the results, generating the HTML for each <li> item
+    for (i=0; i<numberOfShots; i++) {
+        html += '<li>';
+        html += '<a href="'+ data.shots[i].url +'">';
+        html += '<img src="' + data.shots[i].image_url + '" alt="'+ data.shots[i].title +'" />';
+        html += '</a>';
+        html += '</li>';
+    }
+
+    // Insert the generated HTML to the DOM
+    $('.shots').html(html);
+
+    // Set the scroll position of the carousel
+    //var _scrollContainer = $('.shots-container');
+    //var scrollWidth = _scrollContainer.width();
+    //_scrollContainer.scrollLeft(scrollWidth);
+});
